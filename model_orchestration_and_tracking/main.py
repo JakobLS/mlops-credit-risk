08382152bc6e9153c4c53977577030ea03f5522d
Jakob Salomonsson <jakob.salomonsson@gmail.com>
2023-06-18 11:06:44 +0200

    Change run schedule to once per week

--- model_orchestration_and_tracking/main.py
+++ model_orchestration_and_tracking/main.py
@@ -299,8 +299,7 @@ if __name__ == "__main__":
         flow=main,
         name="model_training_and_prediction_daily",
         parameters={'kwargs': {**kwargs}},
-        # schedule=IntervalSchedule(interval=timedelta(minutes=5), timezone='Europe/Madrid'),
-        schedule=CronSchedule(cron="0 3 * * *", timezone="Europe/Madrid"), # Run it at 03:00 am every day
+        schedule=CronSchedule(cron="0 3 * * 1", timezone="Europe/Madrid"), # Run it at 03:00 am every Monday
         infrastructure=Process(working_dir=os.getcwd()), # Run flows from current local directory
         version=1,
         work_queue_name="credit_risk_model_queue",
